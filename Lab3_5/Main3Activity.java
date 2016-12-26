35c5c804d4a1c75d6cfa4b3bfa4edfb60accd371
AnastasiyaKononova <anastkononova@gmail.com>
2016-12-26 21:34:54 +0300

    Lab3_5

---
+++ Lab3_5/Main3Activity.java
@@ -0,0 +1,13 @@
+package com.lab31.admin.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
---
+++ Lab3_5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lab31.admin.myproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+
+    }
+}
